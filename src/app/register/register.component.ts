f9446dc65951b523252d7b305095e31129f6460a
Daniel Boubeta <daniel.boubeta@udc.es>
2023-04-10 19:02:53 +0200

    Added validation on register page

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -11,15 +11,37 @@ export class RegisterComponent {
   
   constructor(formBuilder: FormBuilder) {
     this.form = formBuilder.group({
-      fullName: '',
-      email: '',
-      password: '',
-      confirmPassword: '',
+      fullName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]],
+      confirmPassword: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]],
     });
   }
 
   onRegisterClick() {
+    const password = this.form.value.password;
+    const confirmPassword = this.form.value.confirmPassword;
+    let errors = null;
+    
+    if (password !== confirmPassword) {
+      errors = { 'mismatch': true };
+      this.form.controls['confirmPassword'].setErrors(errors);
+      return
+    }
+    this.form.controls['confirmPassword'].setErrors(errors);
     console.log(this.form.value);
+  }
+
+  showError(controlName: string): boolean {
+    const control = this.form.controls[controlName];
+    if (!control) return false;
+    return control.invalid && (control.touched || control.dirty);
+  }
+
+  getErrorMessage(controlName: string): string {
+    const control = this.form.controls[controlName];
+    if (!control) return "";
+    return JSON.stringify(control.errors);
   }
 
 }
